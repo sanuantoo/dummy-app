7b2ca619da9458ccae0eac03927d00870613403e
sa13596 <sanu.antoo@stud.th-deg.de>
2026-02-04 12:04:43 +0100

    Revert "new button added"
    This reverts commit 9efa7df192bc1344b693d4276f4e6c8c02bfbe32.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,29 +2,7 @@ import './App.css'
 
 function App() {
   return (
-    <div
-      style={{
-        backgroundColor: '#ffffff',
-        height: '100vh',
-        margin: 0,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <button
-        style={{
-          padding: '12px 20px',
-          fontSize: 16,
-          borderRadius: 8,
-          border: '1px solid #333',
-          cursor: 'pointer',
-          background: '#f5f5f5',
-        }}
-      >
-        consumption
-      </button>
-    </div>
+    <div style={{ backgroundColor: '#ffffff', height: '100vh', margin: 0 }} />
   )
 }
 
